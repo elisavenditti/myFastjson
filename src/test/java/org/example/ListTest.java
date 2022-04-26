30607565ae3dc41c69947740e79fb3195c08da43
Elisa <elisavenditti99@gmail.com>
2022-04-26 16:39:41 +0200

    ho aggiunto il sonar-project.properties

--- src/test/java/org/example/ListTest.java
+++ src/test/java/org/example/ListTest.java
@@ -1,2 +1,45 @@
-package org.example;public class ListTest {
+package org.example;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class ListTest extends TestCase {
+
+    @Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][]{{new LinkedList(), 23L,45L,"[23L,45L]"}});
+        // Metodo statico crea l'input fornito all'oggetto testato e descrive l'output atteso
+        // Ritorna un insieme (testedInstance, firstParam, secondParam, expectedResult)
+    }
+    private final List list;                // tested object
+    private final long first;               // first parameter
+    private final long second;              // second parameter
+    private final String expectedResult;    // expected result
+
+    public ListTest(List list, long first, long second, String expectedResult){
+        this.list = list;
+        this.first = first;
+        this.second = second;
+        this.expectedResult = expectedResult;
+    }
+
+    @Test
+    public void test_null() throws Exception {
+        list.add(first);
+        list.add(second);
+        Assert.assertEquals(this.expectedResult, JSON.toJSONString(list, SerializerFeature.WriteClassName));
+    }
+
 }
--- src/test/java/org/example/ParserConfigTest.java
+++ src/test/java/org/example/ParserConfigTest.java
@@ -1,2 +1,54 @@
-package org.example;public class ParserConfigTest {
+package org.example;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class ParserConfigTest extends TestCase {
+
+    @Parameters
+    public static Collection<Object[]> data(){
+        ParserConfig p = new ParserConfig(Thread.currentThread().getContextClassLoader());
+        return Arrays.asList(new Object[][]{{p,"{\"value\":123}", 123}});
+        // Metodo statico crea l'input fornito all'oggetto testato e descrive l'output atteso
+        // Ritorna un insieme (testedInstance, firstParam, expectedResult)
+    }
+
+    private final ParserConfig parserConfig;    // tested object
+    private final String first;                 // provided input
+    private final int expectedResult;           // expected result
+
+
+
+    // Gli argomenti del costruttore sono forniti dal RUNNER che li ottiene invocando il metodo data()
+
+    public ParserConfigTest(ParserConfig parserConfig, String first, int expectedResult){
+        this.parserConfig = parserConfig;
+        this.first = first;
+        this.expectedResult = expectedResult;
+    }
+
+    @Test
+    public void myTest_0(){
+        this.parserConfig.getDeserializers();
+    }
+
+    @Test
+    public void myTest_1() throws Exception{
+        // com.alibaba.json.bvt.serializer.ParserConfigTest.Model model = JSON.parseObject("{\"value\":123}", com.alibaba.json.bvt.serializer.ParserConfigTest.Model.class, this.parserConfig);
+        Model model = JSON.parseObject(this.first, Model.class, this.parserConfig);
+        Assert.assertEquals(this.expectedResult, model.value);
+    }
+
+    public static class Model {
+        public int value;
+    }
 }
